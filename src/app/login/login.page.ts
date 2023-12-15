56af3766ab21a7d0ecebe5c509a769d42e648724
huycarmd <daodh@carmd.com>
2023-12-15 15:31:39 +0700

    change form login

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -4,6 +4,7 @@ import { StorageMap } from '@ngx-pwa/local-storage';
 import { CallAPIService } from '../call-api.service';
 import { LoadingService } from '../loading.service';
 import { Users } from '../shared/users.model';
+import { FormBuilder, FormGroup, FormControl, Validators, FormArray, } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -11,56 +12,49 @@ import { Users } from '../shared/users.model';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage {
-
-  emailVal = ''
-  passwordVal = ''
-  isDisable = false
-  isRemember = false
-  textErrorLogin = ''
-
+  loginForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required]),
+    checkRemember: new FormControl(false),
+  });
 
   constructor(
     private route: ActivatedRoute,
     private router: Router,
-    private storage : StorageMap,
-    private callAPI : CallAPIService,
-    public loading : LoadingService
-  ) {
-
-  }
+    private storage: StorageMap,
+    private callAPI: CallAPIService,
+    public loading: LoadingService
+  ) {}
 
   Login() {
-    this.GetUser()
+    if (this.loginForm.valid) {
+      console.log(this.loginForm.value);
+      this.GetUser()
+    }
   }
 
   GetUser() {
-    if (this.emailVal.search("@") == -1 || this.passwordVal.length < 5){
-      return 
-    } else {
-      this.loading.present()
-      let headers = { 'Content-Type': 'application/json' }; 
-      this.callAPI.callPOSTAPI(
-          'https://dev-api-pro.repairsolutions.com/app1.0/api/users/login',
-          { emailAddress: this.emailVal, password: this.passwordVal },
-          { headers }
-        )
-        .subscribe((response: any) => {
-          let user: Users = response;
-          console.log(user)
-          if (user.message.code == 0) {
-            if (this.isRemember){
-              this.storage.set("Users", response).subscribe((users) => {})
-              console.log("Done save data")
-            }
-            this.emailVal = '';
-            this.passwordVal = '';
-            this.loading.dismiss()
-            this.router.navigate(['/home']);
-          } else {
-            this.textErrorLogin = user.message.description
-            this.loading.dismiss()
+    this.loading.present();
+    let headers = { 'Content-Type': 'application/json' };
+    this.callAPI
+      .callPOSTAPI(
+        'https://dev-api-pro.repairsolutions.com/app1.0/api/users/login',
+        { emailAddress: this.loginForm.value.email, password: this.loginForm.value.password },
+        { headers }
+      )
+      .subscribe((response: any) => {
+        let user: Users = response;
+        console.log(user);
+        if (user.message.code == 0) {
+          if (this.loginForm.value.checkRemember) {
+            this.storage.set('Users', response).subscribe((users) => {});
+            console.log('Done save data');
           }
-        });
-    }
+          this.loading.dismiss();
+          this.router.navigate(['/home']);
+        } else {
+          this.loading.dismiss();
+        }
+      });
   }
 }
